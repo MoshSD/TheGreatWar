fab1b0d7a07d4fc913b14d8ee32f8f3d64dc3810
MoshSD <62759350+MoshSD@users.noreply.github.com>
2024-02-24 17:06:18 +0000

    - Added functionality to query navMesh for specific shapes
    - need to implement way of gathering all objects in a given amount of space (quadtree) and reduce the amount of checks needed to determine whether a part of the world is walkable or not by the A.I as it is N^2 currently

--- SandSim2000/TheGreatWar.cpp
+++ SandSim2000/TheGreatWar.cpp
@@ -1,5 +1,9 @@
 #include "TheGreatWar.h"
 
+
+sf::RectangleShape TheGreatWar::GridSquares[60][60];
+
+
 int main() {
     
     while (TheGreatWar::window.isOpen()) {
@@ -13,7 +17,8 @@ int main() {
         TheGreatWar::window.clear(sf::Color::Black);
 
         TheGreatWar::SpawnRect(sf::Vector2f(100, 100), sf::Vector2f(TheGreatWar::window.getSize().x, TheGreatWar::window.getSize().y / 2), sf::Color::Red, sf::Color::Red);
-        TheGreatWar::SpawnRect(sf::Vector2f(50, 100), sf::Vector2f(TheGreatWar::window.getSize().x / 2, TheGreatWar::window.getSize().y / 2), sf::Color::Transparent, sf::Color::Blue);
+        
+        TheGreatWar::SpawnRect(sf::Vector2f(50, 50), sf::Vector2f(0, 0), sf::Color::Magenta, sf::Color::Magenta, 0);
 
         int numYellowSquares = 5;
         sf::RectangleShape yellowSquare(sf::Vector2f(20, 20));
@@ -28,7 +33,7 @@ int main() {
         TheGreatWar::InitializeNavMesh();
 
 
-
+        
 
         TheGreatWar::window.display();
     }
@@ -46,14 +51,31 @@ void TheGreatWar::SpawnRect(sf::Vector2f dimensions, sf::Vector2f position, sf::
     TheGreatWar::window.draw(tempSquare);
 }
 
+sf::RectangleShape TheGreatWar::SpawnRectangleReturn(sf::Vector2f dimensions, sf::Vector2f position, sf::Color color, sf::Color outlineColor, int outlineThickness)
+{
+    sf::RectangleShape tempSquare(sf::Vector2f(dimensions.x, dimensions.y));
+    tempSquare.setPosition(position.x - tempSquare.getSize().x / 2, position.y - tempSquare.getSize().y / 2);
+    tempSquare.setFillColor(color);
+    tempSquare.setOutlineThickness(outlineThickness);
+    tempSquare.setOutlineColor(outlineColor);
+    TheGreatWar::window.draw(tempSquare); 
+    return tempSquare;
+}
+
 void TheGreatWar::InitializeNavMesh()
 {
+    sf::RectangleShape BlueRectangle = TheGreatWar::SpawnRectangleReturn(sf::Vector2f(50, 100), sf::Vector2f(TheGreatWar::window.getSize().x / 2, TheGreatWar::window.getSize().y / 2), sf::Color::Transparent, sf::Color::Blue, 1);
     // 2D array to initialize grid
-    for (int x = 0; x < 20; x++)
+    for (int x = 0; x < 60; x++)
     {
-        for (int y = 0; y < 20; y++)
+        for (int y = 0; y < 60; y++)
         {
-            TheGreatWar::SpawnRect(sf::Vector2f(m_CellSize, m_CellSize), sf::Vector2f(x * m_CellSize, y * m_CellSize), sf::Color::Transparent, sf::Color::White, 1);
+            TheGreatWar::GridSquares[x][y] = TheGreatWar::SpawnRectangleReturn(sf::Vector2f(m_CellSize, m_CellSize), sf::Vector2f(x * m_CellSize, y * m_CellSize), sf::Color::Transparent, sf::Color::White, 1);
+            if (TheGreatWar::GridSquares[x][y].getGlobalBounds().intersects(BlueRectangle.getGlobalBounds()))
+            {
+                TheGreatWar::GridSquares[x][y].setFillColor(sf::Color::Red);
+                TheGreatWar::window.draw(TheGreatWar::GridSquares[x][y]);
+            }
         }
     }
 
--- SandSim2000/TheGreatWar.h
+++ SandSim2000/TheGreatWar.h
@@ -4,15 +4,18 @@
 #include "InputStateManager.h"
 #include <SFML/Graphics.hpp>
 
+
+
 class TheGreatWar {
 public:
-	static void SpawnRect(sf::Vector2f dimensions, sf::Vector2f position, sf::Color color, sf::Color outlineColor, int outlineThickness = 5);
+	static void SpawnRect(sf::Vector2f dimensions, sf::Vector2f position, sf::Color color, sf::Color outlineColor, int outlineThickness = 0);
+	static sf::RectangleShape SpawnRectangleReturn(sf::Vector2f dimensions, sf::Vector2f position, sf::Color color, sf::Color outlineColor, int outlineThickness = 0);
 	static sf::RenderWindow window;
 	static void InitializeNavMesh();
 
 	static const int m_MeshSize = 2000;
-	static const int m_CellSize = 100;
-	sf::RectangleShape GridSquares[20][20];
+	static const int m_CellSize = 50;
+	static sf::RectangleShape GridSquares[60][60];
 
 private:
 
@@ -20,5 +23,6 @@ private:
 protected:
 
 };
+
 
 sf::RenderWindow TheGreatWar::window(sf::VideoMode::getDesktopMode(), "AI Test Window", sf::Style::Fullscreen);
